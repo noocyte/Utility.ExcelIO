879c4a9c2eb2e6f31d1de7fd8a2177a0c8aedd2f
noocyte <jarle.nygaard@outlook.com>
2014-01-24 13:21:04 +0100

    Added nuspec file

--- src/noocyte.Utility.ExcelIO/noocyte.Utility.ExcelIO/GenericReadExcel.cs
+++ src/noocyte.Utility.ExcelIO/noocyte.Utility.ExcelIO/GenericReadExcel.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using OfficeOpenXml;
 
 namespace noocyte.Utility.ExcelIO
 {
     public class GenericReadExcel
     {
+        public bool HasHeader { get; set; }
+
+        public bool ExtractHeader { get; set; }
+
+        public IDictionary<string, IEnumerable<T>> ExtractRows<T>(Stream stream, Func<ExcelWorksheetRow, T> rowFunction)
+        {
+            using (var package = new ExcelPackage(stream))
+            {
+                return package.Workbook.Worksheets
+                    .ToDictionary(sheet => sheet.Name, sheet => ExtractRows(stream, rowFunction, sheet.Name));
+            }
+        }
+
         public IEnumerable<T> ExtractRows<T>(Stream stream, Func<ExcelWorksheetRow, T> rowFunction, string sheetName)
         {
             using (ExcelPackage package = new ExcelPackage(stream))
@@ -28,7 +42,7 @@ namespace noocyte.Utility.ExcelIO
         private IEnumerable<T> IterateOverRows<T>(ExcelWorksheet worksheet, Func<ExcelWorksheetRow, T> rowFunction)
         {
             int beginRow = 1;
-            if (this.HasHeader && !this.ExtractHeader)
+            if (HasHeader && !ExtractHeader)
                 beginRow = 2;
 
             for (int i = beginRow; i <= worksheet.Dimension.End.Row; i++)
@@ -38,9 +52,5 @@ namespace noocyte.Utility.ExcelIO
                 yield return rowFunction(row);
             }
         }
-
-        public bool HasHeader { get; set; }
-
-        public bool ExtractHeader { get; set; }
     }
 }
