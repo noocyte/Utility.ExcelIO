dc6d4422804eb8cfdd9c2d1dca86783edb13381e
noocyte <jarle.nygaard@outlook.com>
2014-01-29 11:18:00 +0100

    Clean

--- noocyte.Utility.ExcelIO/IgnoreForExportAttribute.cs
+++ noocyte.Utility.ExcelIO/IgnoreForExportAttribute.cs
@@ -5,8 +5,5 @@ namespace noocyte.Utility.ExcelIO
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
     internal sealed class IgnoreForExportAttribute : Attribute
     {
-        internal IgnoreForExportAttribute()
-        {
-        }
     }
 }
